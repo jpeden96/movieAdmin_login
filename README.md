15a03901979d26de255c412590455cea35909982
jpeden96 <j.peden96@gmail.com>
2018-02-16 13:59:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Movie Admin Login #
+
+Welcome to my custom CMS system.
+
+Database is exported in db folder.
+
+# Tutorials/Research #
+
+http://php.net/manual/en/reserved.variables.session.php
+https://stackoverflow.com/questions/27863769/timing-a-user-out-after-to-many-unsuccessful-login-attempts
+https://stackoverflow.com/questions/2182511/setting-a-greeting-based-on-users-time-good-morning-good-afternoon
---
+++ admin/phpscripts/login.php
@@ -0,0 +1,59 @@
+<?php
+
+  function logIn($username, $password, $ip) {
+    require_once('connect.php');
+    $username = mysqli_real_escape_string($link, $username);
+    $password = mysqli_real_escape_string($link, $password);
+    $loginstring = "SELECT * FROM tbl_user WHERE user_name='{$username}'"; //
+    //echo $loginstring;
+    $user_set = mysqli_query($link, $loginstring);
+    //echo mysqli_num_rows($user_set);
+    if(mysqli_num_rows($user_set)){
+      $founduser = mysqli_fetch_array($user_set, MYSQLI_ASSOC);
+      $id = $founduser['user_id'];
+      if($password === $founduser['user_pass'] && $row['user_fail'] < 3){ //separating the password and user login function
+
+        //echo $id;
+
+        date_default_timezone_set('America/Toronto'); //setting the timestamp to where ever the user is logging in from
+        $date = date("Y-m-d h:i:s");
+
+        $_SESSION['user_id'] = $id;
+        $_SESSION['user_name'] = $founduser['user_fname'];
+        $_SESSION['user_last'] = $founduser['user_last'];
+        if(mysqli_query($link, $loginstring)){
+          $update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
+          $updatequery = mysqli_query($link, $update);
+
+          $updateLast = "UPDATE tbl_user SET user_last='{$date}' WHERE user_id={$id}";
+          $updateLastQuery = mysqli_query($link, $updateLast);
+        }
+        redirect_to('admin_index.php');
+
+      }else if($founduser['user_fail'] < 3){ //
+        $number = $founduser['user_fail'] +1;
+
+        $failLogin = "UPDATE tbl_user SET user_fail = {$number} WHERE user_id = {$id}";
+        $fail_attempt = mysqli_query($link, $failLogin);
+        echo "did you forget your password?"
+      }
+    } else {
+      $message = "Did you forget your username or password?";
+      return $message;
+    }
+
+    mysqli_close($link);
+  }
+
+  //trying to add a timestamp for last successful login function
+  //
+  // $LoginRS__query=sprintf("SELECT * FROM tablename WHERE USERNAME='%s' ", get_magic_quotes_gpc() ? $loginUsername : addslashes($loginUsername));
+  //
+  // $LoginRS = mysql_query($LoginRS__query, $onpoint) or die(mysql_error());
+  // $query = mysql_fetch_assoc($LoginRS);
+  //
+  // $GLOBALS['LastLogin'] = $query['LastLogin'];
+  //
+  // //Session variable for last login
+  // $_SESSION['LastLogin'] = $query['LastLogin']; */
+?>
